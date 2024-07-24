712ea25afa0e04de88bafc98583c83ec9469bc45
Rod <roddelag@live.com>
2024-07-24 15:00:22 -0600

    font change

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,28 @@
 import styles from "./page.module.css";
 
+
 export default function Home() {
   return (
-    <main className={styles.main}>
-    
-    <h1 className={`${styles.bigText} ${styles.animatedText}`}>METALITO DIGITAL</h1>
-    <h2 className={styles.mediumText}>
-        <p className={styles.spacing}>Buscamos:</p>
-        <ul>
-          <li>Diseño Gráfico</li>
-          <li>Animación</li>
-          <li>Arte Digital</li>
-          <li>Programación Front End</li>
-          <li>Videografía</li>
-        </ul>
-      </h2>
-      <div className={styles.grid}>
-  <a
-    href="mailto:info@metalito.com"
-    className={styles.card}
-  >
-    <h2>
-      Contacto <span>-&gt;</span>
-    </h2>
-    <p>
-      Escribenos un correo
-    </p>
-  </a>
-</div>
-</main>
+    <>
+      <main className={styles.main}>
+        <h1 className={`${styles.bigText} ${styles.animatedText}`}>METALITO DIGITAL</h1>
+        <h2 className={styles.mediumText}>
+          <p className={styles.spacing}>Buscamos:</p>
+          <ul>
+            <li>Diseño Gráfico</li>
+            <li>Animación</li>
+            <li>Arte Digital</li>
+            <li>Programación Front End</li>
+            <li>Videografía</li>
+          </ul>
+        </h2>
+        <div className={styles.grid}>
+          <a href="mailto:info@metalito.com" className={styles.card}>
+            <h2>Contacto <span>-&gt;</span></h2>
+            <p>Escribenos un correo</p>
+          </a>
+        </div>
+      </main>
+    </>
   );
 }
